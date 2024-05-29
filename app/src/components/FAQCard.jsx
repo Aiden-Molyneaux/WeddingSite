f615985b85a0a99ac762a6e38031d73249f786d3
Aiden Molyneaux <aiden@wicket.io>
2024-05-29 12:45:39 -0400

    Update FAQ questions and answers

--- app/src/components/FAQCard.jsx
+++ app/src/components/FAQCard.jsx
@@ -5,11 +5,11 @@ export default function FAQCard({ id, question, answer }) {
   FAQCard.propTypes = {
     id: PropTypes.string.isRequired,
     question: PropTypes.string.isRequired,
-    answer: PropTypes.string.isRequired
+    answer: PropTypes.oneOfType([PropTypes.string, PropTypes.node]).isRequired
   };
 
   return (
-    <div key={id} className='faqCard'>  
+    <div key={id} className='faqCard'>
       <h6>Q: {question}</h6>
       <p>A: {answer}</p>
     </div>
--- app/src/pages/FAQ.jsx
+++ app/src/pages/FAQ.jsx
@@ -4,41 +4,57 @@ import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 import FAQCard from '../components/FAQCard';
 
 export default function FAQ() {
+  const setLocation = () => {
+    window.localStorage.setItem('location', JSON.stringify({ path: '/RSVP' }));
+  };
+
   const faqData = [
     {
+      question: "When is the wedding?",
+      answer: "The wedding ceremony will be held on Saturday, September 20th, 2025 at 4:00pm. Stewart Park (41 Mill St, Perth, ON K7H 1W2). Following the ceremony, there will be a reception at The Stone Cellar (1 Gore St E, Perth, ON K7H 1H7)."
+    },
+    {
+      question: "Do you have any hotel recommendations?",
+      answer: (
+        <>
+          We haven't secured room blocks, but there are a couple of nearby options for your convenience: <br/><br/>Best Western Hotel / Spa<br/>AirBnB
+        </>
+      )
+    },
+    {
       question: "What is the dress code?",
-      answer: "Semi-formal attire is requested. Please no jeans, shorts, or flip-flops."
+      answer: "We kindly request semi-formal attire for the wedding. Please avoid jeans or overly casual wear. We encourage you to wear the colours blush and navy blue, but feel free to wear whatever makes you feel comfortable and festive!"
     },
     {
-      question: "Will there be food and drinks?",
-      answer: "Yes! We will have a full dinner and an open bar."
+      question: "When is the RSVP deadline?",
+      answer: (
+        <span>
+          The RSVP deadline is June 1st. Please let us know if you can attend by using our <a href='/RSVP' onClick={setLocation} className='rsvpSubmit faqRSVP'>RSVP form</a>
+        </span>
+      )
     },
     {
-      question: "Can I bring a plus one?",
-      answer: "If you have been given a plus one, you will see an option to accept it in the RSVP form."
-    },
-    {
-      question: "What is the parking situation?",
-      answer: "There is a parking lot at the venue that is free to use."
-    },
-    {
-      question: "Can I bring my kids?",
+      question: "Are children invited?",
       answer: "We love kids, but this is an adults-only event."
     },
     {
-      question: "What time does the ceremony start?",
-      answer: "The ceremony will begin at 4:00 PM."
+      question: "Can I bring a guest?",
+      answer: "If you have been given a plus one, you will see an option to accept it in the RSVP form."
     },
     {
-      question: "What time does the reception end?",
-      answer: "The reception will end around 12:00 AM."
+      question: "What is the schedule for the day?",
+      answer: "The ceremony will begin at 4:00pm, followed by a cocktail hour from 5:00pm to 6:00pm. The reception will start at 6:00pm and continue into the evening."
     },
     {
-      question: "Can I take pictures?",
-      answer: "We have hired a professional photographer to capture the event, so we ask that you leave the picture-taking to them."
+      question: "Who can I contact with questions?",
+      answer: "If you have any further questions, please feel free to contact Kyra. Her contact email is at the bottom of the page." // or Brooke (on the Wedding day enter number).
     },
     {
-      question: "How did you source such a brilliant website?",
+      question: "What is the wedding hashtag?",
+      answer: "Feel free to share photos and well wishes using our wedding hashtag: #MolyneauxWedding."
+    },
+    {
+      question: "How did you source such a lovely website?",
       answer: "Well, Aiden created everything himself! He's a software developer and wanted to make something special for our wedding guests."
     }
   ];
