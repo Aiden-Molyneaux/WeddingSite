41dbc9f2789eb153c098a9e9e4f3fe768f7da1e9
Aiden Molyneaux <aiden@wicket.io>
2024-11-28 22:35:52 -0500

    update location gifts page

--- app/src/pages/Gifts.jsx
+++ app/src/pages/Gifts.jsx
@@ -7,7 +7,7 @@ export default function Honeymoon() {
       <h3 className='sectionHeader'>Wedding Gifts</h3>
       <img src={portholeImg} alt='Engagement photo' className='portholeImg'/>
       <div className='giftsContent'>
-        <h4 className='sectionText'>We are going to ____, leaving right after the Wedding!</h4>
+        <h4 className='sectionText'>We are going to Punta Cana, Dominican Republic and leaving right after the Wedding!</h4>
         <br/>
         <h4 className='sectionText'>We are so excited to start our live together and grateful to have such supportive friends and family. If you wish to give a gift, we would be thrilled to receive a contribution towards our honeymoon fund.</h4>
         
