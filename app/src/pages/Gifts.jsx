a2d4e9f716065587d95efd01a8b8cbbdb700e6e6
Aiden Molyneaux <aiden@wicket.io>
2025-01-16 14:56:35 -0500

    Add attendee changes

--- app/src/pages/Gifts.jsx
+++ app/src/pages/Gifts.jsx
@@ -7,12 +7,13 @@ export default function Honeymoon() {
       <h3 className='sectionHeader'>Wedding Gifts</h3>
       <img src={portholeImg} alt='Engagement photo' className='portholeImg'/>
       <div className='giftsContent'>
-        <h4 className='sectionText'>We are going to Punta Cana, Dominican Republic and leaving right after the Wedding!</h4>
+        <h4 className='sectionText'>We are going to Punta Cana, Dominican Republic, leaving right after the Wedding!</h4>
         <br/>
-        <h4 className='sectionText'>We are so excited to start our live together and grateful to have such supportive friends and family. If you wish to give a gift, we would be thrilled to receive a contribution towards our honeymoon fund.</h4>
+        <h4 className='sectionText'>We are so excited to start our married life together and grateful for the love and support of our friends and family. 
+          <br/>
+          <br/>If you wish to give a gift, we would be delighted to receive a contribution towards our honeymoon fund. There will be a card box at the wedding receoption for anyone who wishes to give in person. Alternatively, you can send an e-transfer kyra.hoyte@hotmail.com.</h4>
         
       </div>
-      <button className='rsvpSubmit'>Contribute</button>
       
     </div>    
   );
